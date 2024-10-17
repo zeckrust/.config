27603355c4d516fd438c2de122de2ecd0be632a6
Gabriel Bergeron <gabriel.bergeron.protic@gmail.com>
2024-10-17 16:24:04 -0400

    added config files

---
+++ sway/killall_extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+killall swaybg
+killall waybar
+killall blueman-applet
+
+sleep 0.5
---
+++ sway/start_extensions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+waybar &
+swaybg -i ~/.config/swaybg/arch_background.jpg -m fill &
+blueman-applet &
+
+sleep 0.5
